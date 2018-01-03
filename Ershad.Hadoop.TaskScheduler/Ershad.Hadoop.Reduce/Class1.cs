47b473602991a663b990a3793ed75d8590474e50
osmanrahimi <mr.osmanrahimi@gmail.com>
2018-01-03 20:11:14 +0330

    Init

---
+++ Ershad.Hadoop.TaskScheduler/Ershad.Hadoop.Reduce/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ershad.Hadoop.Reduce
+{
+    public class Class1
+    {
+    }
+}
---
+++ Ershad.Hadoop.TaskScheduler/Ershad.Hadoop.TaskScheduler/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ershad.Hadoop.TaskScheduler
+{
+    public class Class1
+    {
+    }
+}
